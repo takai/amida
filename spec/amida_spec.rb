4e4ccd4ef5d3c291f697015f99662298d26767a7
Naoto Takai <takai@recompile.net>
2012-06-02 14:45:27 +0900

    Refactor spec

--- spec/amida_spec.rb
+++ spec/amida_spec.rb
@@ -3,22 +3,22 @@ require 'spec_helper'
 module Amida
   describe App do
     describe '#header' do
-      it 'returns header' do
+      it 'returns string for header' do
         subject.header(5).should eq "A  B  C  D  E\n|  |  |  |  |  "
       end
     end
+
     describe '#row' do
-      it do
-        subject.stub!(:rand).and_return(2)
+      before { subject.stub!(:rand).and_return(2) }
+      it 'returns string for row' do
         subject.row(5).should eq "|  |  |--|  |  "
       end
     end
 
     describe '#footer' do
-      it do
-        subject.stub!(:rand).and_return(1)
+      before { subject.stub!(:rand).and_return(1) }
+      it 'returns string for footer' do
         subject.footer(5).should eq "|  |  |  |  |  \n   *         "
-
       end
     end
   end
