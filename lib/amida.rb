2d577083df251ebd329272661db6f0684524dd02
Naoto Takai <takai@recompile.net>
2012-06-02 14:50:24 +0900

    Refactor Amida::App

--- lib/amida.rb
+++ lib/amida.rb
@@ -5,25 +5,20 @@ module Amida
     START_CHAR = 65
 
     def header(column)
-      chars = (START_CHAR...(START_CHAR + column)).map(&:chr)
-      chars.join('  ') + "\n" +
-          bar_row_string(column)
+      header_string = (START_CHAR...(START_CHAR + column)).map(&:chr).join('  ')
+      "#{header_string}\n#{bar_row_string(column)}"
     end
 
     def row(column)
       bar = rand(column - 1)
       (0...column).each_with_object("") do |i, r|
-        r << if i == bar
-               "|--"
-             else
-               "|  "
-             end
+        r << (i == bar ? "|--" : "|  ")
       end
     end
 
     def footer(column)
-      win_rows = ([" "] * column).tap { |r| r[rand(column)] = "*" }
-      "#{bar_row_string(column)}\n#{win_rows.join('  ')}"
+      footer_string = ([" "] * column).tap { |r| r[rand(column)] = "*" }.join('  ')
+      "#{bar_row_string(column)}\n#{footer_string}"
     end
 
     def run
